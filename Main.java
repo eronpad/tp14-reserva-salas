91696a3b8208c8d8bc0530cae176023f33f80f85
Eron Padilla <erongpoliveira@gmail.com>
2026-09-01 14:58:28 -0300

    Salas e Horarios feitos

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main{
     public static void main (String[] args)
     {
-        
-    }
+        Matriz minhaMatriz = new Matriz();
+
+        minhaMatriz.carregarSalas();
+        minhaMatriz.carregarHorarios();
+        minhaMatriz.imprimirMatriz();
+    
+}
 }
